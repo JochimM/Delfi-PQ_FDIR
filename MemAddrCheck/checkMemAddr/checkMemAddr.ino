8620800e3d34b469e7dd50b6ce912975db97072c
alexandervdb1995 <a.f.vandenberghe@student.tudelft.nl>
2018-04-24 19:48:00 +0200

    added a test function for the checkIfMemAddress function, this one works, but it doesn't in the main

---
+++ MemAddrCheck/checkMemAddr/checkMemAddr.ino
@@ -0,0 +1,49 @@
+boolean memReceived = false;
+int memAddress;
+char memAddressTry[] = "MA1276x";
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(13, OUTPUT); // the onboard LED
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  checkIfMemAddress(memAddressTry);
+  Serial.println(memAddress);
+  
+  delay(1000);
+}
+
+void checkIfMemAddress(char arr[]){
+  int arrLength = 4;
+  Serial.println(arrLength);
+  char memAddressCh[arrLength];
+  if ((arr[0] == 'M') && (arr[1] == 'A')){
+    //blinkLED(1);
+    byte n = 2;
+    boolean notDone = true;
+    while(notDone) {
+      if (arr[n] != 'x'){
+        memAddressCh[n-2] = arr[n];
+        n ++;
+      }
+      else {
+        notDone = false;
+        }
+      //blinkLED(arr[n]);
+      //delay(1000);
+      }
+    //Serial.println(memAddressCh);
+    //memAddressCh[-1] = NULL;
+    memReceived = true;
+    memAddress = atoi(memAddressCh);
+    //blinkLED(5);
+    //dataToPC(memAddressCh);
+    }
+  else {
+    memReceived = false;
+ 
+    }
+}
